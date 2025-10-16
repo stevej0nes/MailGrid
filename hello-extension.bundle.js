4fc53c5ebf6c09875dc81dd90bf1ad0a5c9cf246
stevej0nes <99020623+stevej0nes@users.noreply.github.com>
2025-10-16 23:11:19 +0100

    Add files via upload

--- hello-extension.bundle.js
+++ hello-extension.bundle.js
@@ -1,33 +1,12 @@
-var helloExtension = (() => {
-  var __defProp = Object.defineProperty;
-  var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-  var __getOwnPropNames = Object.getOwnPropertyNames;
-  var __hasOwnProp = Object.prototype.hasOwnProperty;
+(() => {
   var __require = /* @__PURE__ */ ((x) => typeof require !== "undefined" ? require : typeof Proxy !== "undefined" ? new Proxy(x, {
     get: (a, b) => (typeof require !== "undefined" ? require : a)[b]
   }) : x)(function(x) {
     if (typeof require !== "undefined") return require.apply(this, arguments);
     throw Error('Dynamic require of "' + x + '" is not supported');
   });
-  var __export = (target, all) => {
-    for (var name in all)
-      __defProp(target, name, { get: all[name], enumerable: true });
-  };
-  var __copyProps = (to, from, except, desc) => {
-    if (from && typeof from === "object" || typeof from === "function") {
-      for (let key of __getOwnPropNames(from))
-        if (!__hasOwnProp.call(to, key) && key !== except)
-          __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
-    }
-    return to;
-  };
-  var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
 
   // wwwroot/js/hello-extension.js
-  var hello_extension_exports = {};
-  __export(hello_extension_exports, {
-    default: () => hello_extension_default
-  });
   var import_ui_editor_extensions = __require("@stripoinc/ui-editor-extensions");
   var helloExtension = new import_ui_editor_extensions.ExtensionBuilder().addBlock({
     name: "hello-block",
@@ -45,6 +24,6 @@ var helloExtension = (() => {
       ]
     })
   }).build();
-  var hello_extension_default = helloExtension;
-  return __toCommonJS(hello_extension_exports);
+  window.helloExtension = helloExtension;
+  console.log("\u2705 MailGrid helloExtension loaded:", window.helloExtension);
 })();
